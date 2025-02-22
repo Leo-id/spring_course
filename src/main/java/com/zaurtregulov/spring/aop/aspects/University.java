416cf5df30d3ed05e49fe35afb45802cb1777eb9
Константин Леончик <k.leonchik@inbox.ru>
2025-02-22 21:45:13 +0300

    lesson 32

--- src/main/java/com/zaurtregulov/spring/aop/aspects/University.java
+++ src/main/java/com/zaurtregulov/spring/aop/aspects/University.java
@@ -1,4 +1,11 @@
 package com.zaurtregulov.spring.aop.aspects;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class University {
+    private List<Student> students = new ArrayList<>();
 }
